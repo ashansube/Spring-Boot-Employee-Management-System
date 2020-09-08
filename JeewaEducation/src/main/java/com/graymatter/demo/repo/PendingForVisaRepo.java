447c23c0c55afd33d0057e6c1efe4ec5f21ec847
pramitha1166 <pramitha.wanigarathne@gmail.com>
2020-09-08 11:20:53 +0530

    dsads

--- JeewaEducation/src/main/java/com/graymatter/demo/repo/PendingForVisaRepo.java
+++ JeewaEducation/src/main/java/com/graymatter/demo/repo/PendingForVisaRepo.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.graymatter.demo.model.PendingForVisa;
-#dsds
+
 @Repository
 public interface PendingForVisaRepo extends JpaRepository<PendingForVisa, Integer> {
 
