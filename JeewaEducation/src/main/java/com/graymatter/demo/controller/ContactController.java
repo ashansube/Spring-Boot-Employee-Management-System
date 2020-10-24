c9bdc83ad840171d73a08693e2c0bc6fc174a8d6
Ashan <37544078+Ashaxn@users.noreply.github.com>
2020-10-24 20:44:58 +0530

    Final Settings

--- JeewaEducation/src/main/java/com/graymatter/demo/controller/ContactController.java
+++ JeewaEducation/src/main/java/com/graymatter/demo/controller/ContactController.java
@@ -54,7 +54,7 @@ public class ContactController {
 		mailContent += "<hr><img src='cid:logoImage' />";
 		
 		helper.setFrom("ashan.javatest@gmail.com", "Jeewa Employees");
-		helper.setTo("ashan.subawickrama12@gmail.com");
+		helper.setTo("example@gmail.com"); // Add you email name
 		helper.setSubject(mailSubject);
 		helper.setText(mailContent, true);
 		
